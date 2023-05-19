fd04487955367524b857b35fb0b33922ac748c88
Cynthia <ladypenguin1991@gmail.com>
2023-05-19 15:35:08 -0500

    switching to chatbox branch. setup complete

---
+++ client/src/components/chatbox.jsx
@@ -0,0 +1,3 @@
+import React, { useState, useEffect } from 'react';
+import { TextField, Button, Box } from '@mui/material';
+
---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, { cors: { origin: '*' } });
+
+app.get('/', (req, res) => {
+  res.sendFile('/home/useradd/Senior/wheel-of-love/index.html');
+});
+
+server.listen(3001, () => {
+  console.log('listening on localhost:3001');
+});
+
+//building socket.io logic
+//event emitter to check for connection
+io.on('connection', (socket) => {
+  //socket event creation
+  console.log('socket id', socket.id);
+  //socket method
+  socket.on('message', (data) => {
+    console.log(data);
+    //emit data to everyone but self
+    // socket.emit(data);
+    socket.broadcast.emit('message', data);
+  });
+});
